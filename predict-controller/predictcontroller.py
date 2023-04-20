277a7607185993d976f2466f33ef0a3f92705918
clphan <pdkhanh06@gmail.com>
2023-04-20 21:13:38 +0700

    Fix

--- predict-controller/predictcontroller.py
+++ predict-controller/predictcontroller.py
@@ -5,12 +5,22 @@ from datetime import timedelta
 from prometheus_api_client import PrometheusConnect
 from prometheus_api_client.utils import parse_datetime
 
+# Load the Excel file into a Pandas DataFrame
+df = pd.read_excel('wc_dataset_processed.csv')
+df['event_time'] = pd.to_datetime(df['event_time'])
+df = df.set_index('event_time')
+
+
 # Connect to Prometheus server
 prom = PrometheusConnect(url="http://prometheus.khanh-thesis.online", disable_ssl=True)
 
 start_time = parse_datetime("9minutes")
 end_time = parse_datetime("now")
 step = "1m"
+def find_events(timestamp):
+    num_match_event = df.loc[timestamp, 'num_match_event']
+    print(num_match_event)
+
 def get_metrics(start_time, end_time, step):
     metric_data_request_count = prom.custom_query_range(
         query='sum(rate(istio_requests_total{source_app="request-simulate", destination_app="appsimulate", response_code="200", connection_security_policy="unknown", job="envoy-stats"}[1m])) * 50',
@@ -39,3 +49,12 @@ def get_metrics(start_time, end_time, step):
 
 rs = get_metrics(start_time, end_time, step)
 print(rs)
+
+# past_trigger_time = "1998-04-30 21:45:00"
+# current_trigger_time = "2034-04-30 21:45:00"
+# time_offset = past_trigger_time - current_trigger_time
+
+# past_read_timedata = current_read_timedata - time_offset
+
+# # get match value in excel match the time
+# past_read_timedata = "1998-04-30 21:45:00"
--- system/ops/script-create.sh
+++ system/ops/script-create.sh
@@ -3,11 +3,12 @@
 aws eks update-kubeconfig --name app --region ap-southeast-1
 alias k=kubectl
 k create ns external-dns
-k create ns istio-istio
+k create ns istio-system
 k create ns istio-ingress
 k create ns kiali-operator
 k create ns appsimulate
 k create ns request-simulate
+k create ns predict-controller
 
 cd external-dns
 helm install -f values.yaml external-dns . -n external-dns 
@@ -17,6 +18,7 @@ cd istio
 helm install -f values.yaml istio-system . -n istio-system 
 cd ..
 
+sleep 10
 cd istio-ingress 
 helm install -f values.yaml istio-ingress . -n istio-ingress 
 cd ..
@@ -29,16 +31,16 @@ cd ..
 # helm install -f values.yaml kiali-operator --set cr.create=true . -n kiali-operator
 # cd ..
 
-cd istio_monitor
+cd istio-monitor
 k apply -f . 
 cd ..
 
 cd ../..
-cd appsimulate/deploy
+cd app-simulate/deploy
 k apply -f . 
 
 cd ../..
-cd request_simulate/deploy 
+cd request-simulate/deploy 
 k apply -f .
 
 cd ../..
